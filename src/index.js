f0d8c01d3109e10f21d0f822e62e8864ad3b91f2
Hamza Nasir <hamzanasir123123@gmail.com>
2020-10-03 18:31:21 +0500

    Changed index.js in attempt to check out branches

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/*<App />*/}
+    <div>Hello World</div>
   </React.StrictMode>,
   document.getElementById('root')
 );
